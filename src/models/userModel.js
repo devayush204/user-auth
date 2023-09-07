2b3e6c65f4f7cf40d73adefde0dfcb8c64d7064d
User <your@email.com>
2023-09-07 18:45:31 +0530

    2nd commit

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,18 +16,18 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: [true, "please provide a password"],
         },
-        // isVerified: {
-        //     type: Boolean,
-        //     default: false
-        // },
-        // isAdmin: {
-        //     type: Boolean,
-        //     default: false
-        // },
-        // forgotPasswordToken: String,
-        // forgotPasswordTokenExpiry: Date,
-        // verifyToken: String,
-        // verifyTokenExpiry: Date
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date
 
 })
 
